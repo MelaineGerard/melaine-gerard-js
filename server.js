e15c4c1084996adae2dfc50c8207efeac359819e
Melaine Gérard <pro@melaine-gerard.fr>
2021-06-22 14:47:35 +0200

    Added mysql connection

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const config = require('./config.json'),
     express = require('express'),
     app = express(),
     path = require('path'),
+    mysql = require('mysql'),
+    db = mysql.createConnection({
+        host: config.mysql.host,
+        user: config.mysql.user,
+        password: config.mysql.password,
+        port: config.mysql.port,
+        database: config.mysql.dbName,
+    }),
     hbs = require('hbs'),
     md = require("markdown-it")();
 
 
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
-
+app.use(express.static('public'));
 app.set('view engine', 'hbs');
+
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+});
+
 
 app.get('/', (request, response) => {
     response.render('index', {
@@ -19,6 +33,6 @@ app.get('/', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(config.port, () => {
     console.log(`Server started on port ${config.port}`)
 })
